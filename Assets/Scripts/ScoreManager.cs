93d5bd1d675c03c2ca2b69016e0fa68db7ae7e29
Ogunbayik <ogun.bayikk@gmail.com>
2024-03-15 23:46:27 +0300

    Fix ScoreManager

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,9 +8,12 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
 
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI gemText;
 
     [HideInInspector]
     public int currentScore;
+    [HideInInspector]
+    public int currentGemCount;
     private void Awake()
     {
         if (instance != null && instance != this)
@@ -21,12 +24,20 @@ public class ScoreManager : MonoBehaviour
     void Start()
     {
         currentScore = 0;
+        currentGemCount = 0;
+
         scoreText.text = "Score: " + currentScore;
+        gemText.text = currentGemCount.ToString();
     }
 
     public void AddScore(int score)
     {
         currentScore += score;
         scoreText.text = "Score: " + currentScore;
+    }
+
+    public void AddGem(int count)
+    {
+        currentGemCount += count;
     }
 }
